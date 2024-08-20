22603245f544ef1e7a0e54d1ad7c23b490cf84fb
unknown <mbardakov@gmail.com>
2024-08-19 21:45:35 -0400

    use unit tests to assert that test data is being imported successfully

--- test/test.js
+++ test/test.js
@@ -2,9 +2,10 @@ import { strict as assert } from 'assert';
 import data from './data.js';
 
 describe('data', function () {
-  describe('data does exist', function () {
-    it('objects should have at least one key', function () {
-      assert.notEqual(Object.keys(data), 0);
-    });
+  it('can be imported', function () {
+    assert.deepStrictEqual(Object.keys(data), ['profile', 'topTracks', 'trackFeatures']);
+    assert.equal(!!(data.profile), true);
+    assert.equal(!!(data.topTracks), true);
+    assert.equal(!!(data.trackFeatures), true);
   });
 });
